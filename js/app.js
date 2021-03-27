4c6125d64b05124ee34ea565bc1287fc9b47a875
JD <a920423588@gmail.com>
2021-03-27 20:05:42 +0000

    m

--- js/app.js
+++ js/app.js
@@ -6,39 +6,38 @@ new Vue({
             loading: false,
             user_input: '',
             video_source: 'all',
-            video_date: ''
+            video_from_date: '',
+            video_to_date: '',
+            least_like_count: '',
+            least_view_count: '',
+            sort_by: ''
+
         }
     },
     methods:{
         GetUserInput(){
-            alert(this.user_input)
-            const article ={user_input: this.user_input, video_source: this.video_source, video_date: this.video_date}
-            axios.post('http://localhost:8080',article)
+            this.video_list = [];
+            const article ={user_input: this.user_input, 
+                            video_source: this.video_source, 
+                            video_from_date: this.video_from_date, 
+                            video_to_date: this.video_to_date,
+                            least_like_count: this.least_like_count,
+                            least_view_count: this.least_view_count,
+                            sort_by: this.sort_by};
+            
+            axios.post('http://35.239.68.120:8080/',article)
             .then((response) => {
                 this.video_list = response.data;
-                window.location.href="./search.html";
                 this.user_input = '';
             })
         },
         GetVideo(){
             this.loading = true;
-            axios.get('http://localhost:8080')
+            axios.get('http://35.239.68.120:8080/')
             .then((response) => {
                 this.video_list = response.data;
                 this.loading = false;
             });
-        },
-        BiliBiliSource(){
-            this.video_source = 'bilibili'
-        },
-        TiktokSouce(){
-            this.video_source = 'tiktok'
-        },
-        YoutubeSouce(){
-            this.video_source = 'youtube'
-        },
-        Allsource(){
-            this.video_source = 'all'
         }
     }
 })
