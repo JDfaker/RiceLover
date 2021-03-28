738e72aff727f23b9f60e0dae4e29539e497cccc
JD <a920423588@gmail.com>
2021-03-28 12:40:44 +0100

    add pre-value, add background

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ new Vue({
         return {
             video_list:[],
             loading: false,
-            user_input: '',
+            user_input: 'apple',
             video_source: 'all',
-            video_from_date: '',
-            video_to_date: '',
-            least_like_count: '',
-            least_view_count: '',
-            sort_by: ''
+            video_from_date: '2000-1-1',
+            video_to_date: '2021-1-1',
+            least_like_count: '0',
+            least_view_count: '0',
+            sort_by: 'likeCount'
 
         }
     },
