83170db090910089f861b610c0a2c43103c02fb2
JD <a920423588@gmail.com>
2021-03-31 11:38:09 +0100

    change time

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ new Vue({
             sort_by: 'likeCount',
             comprehensive_sort: 'disable',
 
-            number_of_show: 2,
+            number_of_show: 10,
             show_view_more: false,
 
             time_cost: 0,
@@ -22,9 +22,8 @@ new Vue({
     },
     methods:{
         GetUserInput(){
-            alert(this.comprehensive_sort)
             this.video_list = [];
-            this.number_of_show = 2;
+            this.number_of_show = 10;
             const article ={user_input: this.user_input, 
                             video_source: this.video_source, 
                             video_from_date: this.video_from_date, 
@@ -40,13 +39,23 @@ new Vue({
                 this.user_input = '';
                 this.time_used = this.video_list[0].time_cost;
                 this.num_of_result = this.video_list[0].amount;
+
+                for(var i=0; i<this.video_list.length;i++){
+                    var date = new Date(this.video_list[i].published);
+                    var date_str = '';
+                    Y = date.getFullYear() + '-';
+                    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+                    D = date.getDate();
+                    date_str = Y+M+D
+                    this.video_list[i].published = date_str;
+                }
             })
             //junmp to id result_point
             document.querySelector("#result_point").scrollIntoView(true);
             this.show_view_more = true;
         },
         AddShowNumber(){
-            this.number_of_show = this.number_of_show + 2
+            this.number_of_show = this.number_of_show + 10
         }
     }
 })
